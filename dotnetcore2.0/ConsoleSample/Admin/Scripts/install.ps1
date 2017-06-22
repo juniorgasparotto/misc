3c2740fbc352889af523da4890bf1b5d184ae2a9
Glauber Gasparotto <glauberj@ciandt.com>
2017-06-22 17:39:43 -0300

    .

--- dotnetcore2.0/ConsoleSample/Admin/Scripts/install.ps1
+++ dotnetcore2.0/ConsoleSample/Admin/Scripts/install.ps1
@@ -1,3 +1,15 @@
 param ([Parameter(Mandatory=$true)][string]$find,[Parameter(Mandatory=$true)][string]$projectName)
-gci -r -include "*.cs","*.sln","*.csproj","*.json" | foreach-object { $a = $_.fullname; ( get-content $a ) |
-  foreach-object { $_ -replace $find,$projectName } | set-content $a }
+gci -r -include "*.cs","*.sln","*.csproj","*.json" | 
+foreach-object {
+  $a = $_.fullname;
+  $file = Get-Content $_.FullName
+  echo $a;
+  $containsWord = $file | %{$_ -match $find};
+
+  If($containsWord -contains $true) {
+    (get-content $a)  |
+    foreach-object {
+      $_ -replace $find,$projectName 
+    } | set-content $a 
+  }
+}
