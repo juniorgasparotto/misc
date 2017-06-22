d2e32aad61184d4a75602dd58460f6537d718153
Glauber Gasparotto <glauberj@ciandt.com>
2017-06-22 18:22:05 -0300

    .

--- dotnetcore2.0/ConsoleSample/Admin/Scripts/install.bat
+++ dotnetcore2.0/ConsoleSample/Admin/Scripts/install.bat
@@ -1,31 +1,39 @@
-@echo off
+@echo on
 setlocal
 :PROMPT
 SET /P AREYOUSURE="Are you sure you want to install/reinstall this project? In case of reinstallation, this process will exclude all data, including the database. (y/[n])? "
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+set /p currentName=<admin\config
 
 :ENTER
 echo.
 SET /P projName="Project name: "
 IF /I "%projName%" == "" GOTO ENTER
 
-echo "Deleting database if exists..."
-dotnet ef database drop -f
+echo.
+echo Installing...
 
-echo "Deleting 'bin', 'obj', 'Migrations' folder if exists..."
+PowerShell.exe -ExecutionPolicy RemoteSigned -File admin\Scripts\install.ps1 "%currentName%" "%projName%"
+echo %projName%>admin\config
+
+echo.
+echo Deleting 'bin', 'obj', 'Migrations' folder if exists...
 
 IF EXIST "obj" rd /s /q "obj"
 IF EXIST "bin" rd /s /q "bin"
 IF EXIST "Migrations" rd /s /q "Migrations"
 
-echo "Installing..."
-
-
-
-PowerShell.exe -ExecutionPolicy RemoteSigned -File admin\Scripts\install.ps1 "ConsoleSample" "%projName%"
-
+echo.
+echo Rename to %projName%...
+ren "%currentName%.csproj" "%projName%.csproj"
+pause
+echo.
 echo "Restore packages..."
 dotnet restore
+pause
+echo "Deleting database if exists..."
+dotnet ef database drop -f
 
 echo "Creating database..."
 
